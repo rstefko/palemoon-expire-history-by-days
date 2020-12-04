845d765cab3bc9ca348fd6e6581ccbcb23cac7de
Roman Stefko <roman.stefko@eway-crm.com>
2020-12-04 14:47:27 +0100

    Removed Firefox support

--- pack.bat
+++ pack.bat
@@ -1,2 +1,2 @@
 cd src
-"C:\Program Files\7-Zip\7z.exe" a -tzip ..\bin\palemoon_expire_history_by_days-1.2.2-fx.xpi *
+"C:\Program Files\7-Zip\7z.exe" a -tzip ..\bin\palemoon_expire_history_by_days-1.2.3-fx.xpi *
